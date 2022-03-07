ad408d9ba5284ef793ad620313384e64003456ef
akilasar <77478803+akilasar@users.noreply.github.com>
2022-03-07 14:11:17 -0500

    Update color_segmentation.py

--- scripts/computer_vision/color_segmentation.py
+++ scripts/computer_vision/color_segmentation.py
@@ -35,9 +35,26 @@ def cd_color_segmentation(img, template):
 	"""
 	########## YOUR CODE STARTS HERE ##########
 
-	bounding_box = ((0,0),(0,0))
+	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	COLOR_MIN = np.array([0, 160, 155],np.uint8)
+	COLOR_MAX = np.array([25, 255, 255],np.uint8)
+	frame_threshed = cv2.inRange(hsv_img, COLOR_MIN, COLOR_MAX)
+	imgray = frame_threshed
+	ret,thresh = cv2.threshold(frame_threshed,127,255,0)
+	_, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+	# Find the index of the largest contour
+	areas = [cv2.contourArea(c) for c in contours]
+	max_index = np.argmax(areas)
+	cnt=contours[max_index]
+
+	x,y,w,h = cv2.boundingRect(cnt)
+	cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+	cv2.imshow("Show",img)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
 
 	########### YOUR CODE ENDS HERE ###########
 
 	# Return bounding box
-	return bounding_box
+	return ((x,y),(x+w,y+h))
